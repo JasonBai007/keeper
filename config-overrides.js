f4e040b0b44c2cb353a73cb25f9b6af99e8b2f90
JasonBai007 <byygkcg@163.com>
2019-11-04 16:00:43 +0800

    自定义样式

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,14 @@
-const { override, fixBabelImports } = require('customize-cra');
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
 
+// 自定义配置cra，并且覆盖antd原始的Less变量
 module.exports = override(
     fixBabelImports('import', {
         libraryName: 'antd',
         libraryDirectory: 'es',
-        style: 'css',
+        style: true,
+    }),
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyVars: { '@primary-color': '#09d3ac' },
     }),
 );
