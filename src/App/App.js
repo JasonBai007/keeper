7a99fbdd720b9bb1d3e5b74ca4932f20c86395aa
JasonBai007 <byygkcg@163.com>
2019-11-07 10:13:41 +0800

    引入github丝带

--- src/App/App.js
+++ src/App/App.js
@@ -29,6 +29,16 @@ class App extends React.Component {
   render() {
     return (
       <div className="wrap">
+        {/* 引入github丝带 */}
+        <a
+          className="github-fork-ribbon"
+          href="https://github.com/JasonBai007/keeper"
+          target="_blank"
+          rel="noopener noreferrer"
+          data-ribbon="Fork me on GitHub"
+        >
+          github-fork-ribbon
+        </a>
         <h2>keeper</h2>
         <TopForm submit={this.renderChart}></TopForm>
         <REcharts onRef={this.onRef}></REcharts>
