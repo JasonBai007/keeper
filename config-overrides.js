2e220cf1f7de77904cf781503ccd3adc06425f4c
JasonBai007 <byygkcg@163.com>
2019-11-07 15:29:53 +0800

    关闭sourcemap

--- config-overrides.js
+++ config-overrides.js
@@ -1,18 +1,26 @@
-const { override, fixBabelImports, addLessLoader, addWebpackAlias } = require('customize-cra');
-const path = require('path');
+const {
+  override,
+  fixBabelImports,
+  addLessLoader,
+  addWebpackAlias
+} = require("customize-cra");
+const path = require("path");
+
+// 关闭sourcemap
+process.env.GENERATE_SOURCEMAP = "false";
 
 // 自定义配置cra，并且覆盖antd原始的Less变量
 module.exports = override(
-    fixBabelImports('import', {
-        libraryName: 'antd',
-        libraryDirectory: 'es',
-        style: true,
-    }),
-    addLessLoader({
-        javascriptEnabled: true,
-        modifyVars: { '@primary-color': '#c94642' },
-    }),
-    addWebpackAlias({
-        "@": path.resolve(__dirname, 'src')
-    }),
+  fixBabelImports("import", {
+    libraryName: "antd",
+    libraryDirectory: "es",
+    style: true
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: { "@primary-color": "#c94642" }
+  }),
+  addWebpackAlias({
+    "@": path.resolve(__dirname, "src")
+  })
 );
