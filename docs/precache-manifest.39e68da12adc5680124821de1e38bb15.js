1210b2e029ea2aeed3322e94b09768bb1244a5cf
JasonBai007 <byygkcg@163.com>
2019-11-07 15:32:03 +0800

    测试上线第一版

---
+++ docs/precache-manifest.39e68da12adc5680124821de1e38bb15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1956e0ae4fbfb56ae28e476dd1949eeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6410e673e0d0143eebb",
+    "url": "/static/css/2.7d3d4f4d.chunk.css"
+  },
+  {
+    "revision": "33a556b8814220ad6843",
+    "url": "/static/css/main.e5b3375b.chunk.css"
+  },
+  {
+    "revision": "f6410e673e0d0143eebb",
+    "url": "/static/js/2.b7abd238.chunk.js"
+  },
+  {
+    "revision": "33a556b8814220ad6843",
+    "url": "/static/js/main.3413cdf7.chunk.js"
+  },
+  {
+    "revision": "8f148e211ea83fb37aa2",
+    "url": "/static/js/runtime-main.ab06e917.js"
+  }
+]);
